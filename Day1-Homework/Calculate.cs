493511ea54e6b3ebbd6564e05ed6c9b3df63dee0
KeN <topken1@hotmail.com>
2016-10-07 02:05:37 +0800

    Remove Function

--- Day1-Homework/Calculate.cs
+++ Day1-Homework/Calculate.cs
@@ -7,25 +7,12 @@ namespace Day1_Homework
 {
     public class Calculate<T>
     {
-        //private int SumOfProperty(IEnumerable<T> source, string propertyName)
-        //{
-        //    int result = 0;
-        //    Type type = typeof(T);
-        //    PropertyInfo propertyInfo = type.GetProperty(propertyName);
-        //    if (propertyInfo != null && propertyInfo.PropertyType == typeof(int))
-        //    {
-
-        //        result = source.Sum(s => (int)propertyInfo.GetValue(s));
-        //    }
-        //    return result;
-        //}
 
         public IEnumerable<int> SumOfPropertyGrupByNumber(IEnumerable<T> source, string propertyName, int groupNumber)
         {
             IEnumerable<int> result = null;
             List<T> listSource = source.ToList();
-            Type type = typeof(T);
-            PropertyInfo propertyInfo = type.GetProperty(propertyName);
+            var propertyInfo = typeof(T).GetProperty(propertyName);
             if (propertyInfo != null && propertyInfo.PropertyType == typeof(int))
             {
                 result = listSource.GroupBy(g => (listSource.IndexOf(g) / groupNumber + 1))
